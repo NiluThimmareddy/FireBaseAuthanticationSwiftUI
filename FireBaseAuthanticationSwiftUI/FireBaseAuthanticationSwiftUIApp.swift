cf3935404ff1975d3dc21d4079eca64f72750a8a
Nilu <niluk3700@gmail.com>
2025-05-03 15:07:30 +0530

    Initial Commit

---
+++ FireBaseAuthanticationSwiftUI/FireBaseAuthanticationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseAuthanticationSwiftUIApp.swift
+//  FireBaseAuthanticationSwiftUI
+//
+//  Created by ToqSoft on 03/05/25.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseAuthanticationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
